718ee82f901ed6f6c3b15214660893cc8a6e8c04
Yuliang Li <yuliang.li-3@postgrad.manchester.ac.uk>
2024-10-25 22:21:12 +0100

    init commit collected image data -> train and use to object detection
    Signed-off-by: Yuliang Li <yuliang.li-3@postgrad.manchester.ac.uk>

---
+++ build/Object_Detection/build/lib/Object_Detection/collect_data.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class ImageDepthSubscriber(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.sub_color = self.create_subscription(
+            Image, '/camera/camera/color/image_raw', self.listener_callback_color, 10
+        )
+        self.sub_depth = self.create_subscription(
+            Image, '/camera/camera/depth/image_rect_raw', self.listener_callback_depth, 10
+        )
+        self.cv_bridge = CvBridge()
+        self.depth_image = None
+
+        self.color_count = 0
+        self.depth_count = 0
+
+    def listener_callback_color(self, data):
+        # self.get_logger().info('Receiving color video frame')
+        color_image = self.cv_bridge.imgmsg_to_cv2(data, 'bgr8')
+
+        file_name = f"/home/mscrobotics2425laptop21/ros2_test/saved_image/bridge_yellow_color_{self.color_count/10}.jpg"
+        if self.color_count%10 == 0:
+            cv2.imwrite(file_name, color_image)
+            self.get_logger().info('Saved color image as {}'.format(file_name))
+        self.color_count += 1
+
+    def listener_callback_depth(self, data):
+        # self.get_logger().info('Receiving depth video frame')
+        depth_image = self.cv_bridge.imgmsg_to_cv2(data, '16UC1')
+
+        file_name = f"/home/mscrobotics2425laptop21/ros2_test/saved_image/bridge_yellow_depth_{self.depth_count/10}.png"
+        if self.depth_count%10 == 0:
+            cv2.imwrite(file_name, depth_image)
+            self.get_logger().info('Saved depth image as {}'.format(file_name))
+        self.depth_count += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageDepthSubscriber("image_depth_sub")
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/Object_Detection/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'Object_Detection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mscrobotics2425laptop21',
+    maintainer_email='yuliang.li-3@postgrad.manchester.ac.uk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'collect_data =''Object_Detection.collect_data:main',
+        ],
+    },
+)
