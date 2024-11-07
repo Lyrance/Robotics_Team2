abe9a065e34fe0b113c1c27dac987acb9f6f05b3
Yuliang Li <lyrance@outlook.com>
2024-11-07 16:25:56 +0000

    adding raw_image, next step -> label and retrain it

---
+++ src/object_detection/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'object_detection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mscrobotics2425laptop21',
+    maintainer_email='yuliang.li-3@postgrad.manchester.ac.uk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'collect_data =''object_detection.collect_data:main',
+            'predict =''object_detection.predict:main',
+            'test =''object_detection.test:main',
+        ],
+    },
+)
