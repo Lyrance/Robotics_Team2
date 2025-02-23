effd15ecc2cb33b1a7f6a374c66438deae6953f1
Hzxxxxxxx <117897346+Hzxxxxxxx@users.noreply.github.com>
2025-02-23 21:28:24 +0000

    Update setup.py

--- src/manipulator/setup.py
+++ src/manipulator/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ServerOfPerceptionAndGrasp = manpulator.server_of_perception_for_grasp:main',
         ],
     },
 )
