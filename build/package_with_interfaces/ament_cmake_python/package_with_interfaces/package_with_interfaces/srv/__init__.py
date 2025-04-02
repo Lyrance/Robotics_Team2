050df1ca956d0301f50d11af72a396172a9d2a15
AvidLuv <1764741747@qq.com>
2025-04-03 00:59:43 +0100

    update controller and color detection

--- build/package_with_interfaces/ament_cmake_python/package_with_interfaces/package_with_interfaces/srv/__init__.py
+++
@@ -1,2 +0,0 @@
-from package_with_interfaces.srv._object_grab import ObjectGrab  # noqa: F401
-from package_with_interfaces.srv._set_rover_state import SetRoverState  # noqa: F401
--- install/package_with_interfaces/local/lib/python3.10/dist-packages/package_with_interfaces/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from package_with_interfaces.msg._object_information import ObjectInformation  # noqa: F401
